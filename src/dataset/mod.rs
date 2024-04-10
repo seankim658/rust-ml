0844cb1efc608cd6b226bc086b11447e37225c7b
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-04-10 01:07:38 -0400

    finished simple base from_csv function

--- src/dataset/mod.rs
+++ src/dataset/mod.rs
@@ -4,12 +4,15 @@
 
 use crate::base::error::{Error, ErrorKind};
 use crate::base::MLResult;
+use crate::linalg::Matrix;
+use crate::linalg::Vector;
 
 use csv::ReaderBuilder;
-use serde::Deserialize;
+use num::Float;
 use std::fmt::Debug;
 use std::fs::File;
 use std::path::Path;
+use std::str::FromStr;
 
 /// Loads the infamous UCI Iris dataset.
 pub mod iris;
@@ -29,7 +32,8 @@ where
 {
     data: X,
     target: Y,
-    columns: Vec<String>,
+    data_columns: Vector<String>,
+    target_column: String,
 }
 
 /// Constructor and some getters for the Dataset struct.
@@ -39,11 +43,12 @@ where
     Y: Clone + Debug,
 {
     /// Constructor.
-    pub fn new(data: X, target: Y, columns: Vec<String>) -> Self {
+    pub fn new(data: X, target: Y, data_columns: Vector<String>, target_column: String) -> Self {
         Dataset {
             data,
             target,
-            columns,
+            data_columns,
+            target_column,
         }
     }
 
@@ -59,31 +64,79 @@ where
 }
 
 // From functions for the dataset struct.
-impl<X, Y> Dataset<X, Y>
+impl<X> Dataset<Matrix<X>, Vector<X>>
 where
-    X: for<'de> Deserialize<'de> + Clone + Debug,
-    Y: for<'de> Deserialize<'de> + Clone + Debug,
+    X: Float + Debug + FromStr,
 {
     /// Creates a Dataset struct from a CSV file. All
     /// features columns have to be of the same, numeric
     /// type. The taret column can be a categorical value
-    /// but it will be automatically one hot encoded.
+    /// but it will be automatically label encoded.
     ///
     /// Parameters
     /// - filepath: A Path reference.
     /// - target_column: The target column name.
     ///
     pub fn from_csv<P: AsRef<Path>>(file_path: P, target_column: &str) -> MLResult<Self> {
-
-        let file = File::open(file_path.as_ref()).map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        let file =
+            File::open(file_path.as_ref()).map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
         // Create the csv Reader from the file (assumes headers are available).
         let mut rdr = ReaderBuilder::new().has_headers(true).from_reader(file);
 
         // Isolate the header row.
-        let headers = rdr.headers().map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
-        // Make sure target column exists in the file data.
-        let target_index = headers.iter().position(|h| h == target_column)
-            .ok_or_else(|| Error::new(ErrorKind::InvalidData, "Target column not found in CSV file: {p}"))?;
+        let headers = rdr
+            .headers()
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
 
+        // Make sure target column exists in the file data.
+        let target_index = headers
+            .iter()
+            .position(|h| h == target_column)
+            .ok_or_else(|| {
+                Error::new(
+                    ErrorKind::InvalidData,
+                    format!("Target column {} not found in CSV file.", target_column),
+                )
+            })?;
+
+        let mut data_rows = Vec::new();
+        let mut target_values = Vec::new();
+        // Build the data rows 2d vector and the label vector.
+        for record_result in rdr.records() {
+            let record = record_result.map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+            let mut record_features = Vec::new();
+            for (index, feature) in record.iter().enumerate() {
+                if index == target_index {
+                    let record_target = X::from_str(feature).map_err(|_| {
+                        Error::new(
+                            ErrorKind::InvalidData,
+                            format!("Failed to parse target value {}", feature),
+                        )
+                    })?;
+                    target_values.push(record_target);
+                } else {
+                    let feature_value = X::from_str(feature).map_err(|_| {
+                        Error::new(ErrorKind::InvalidData, format!("Failed to parse value {} in column {}", feature, index))
+                    })?;
+                    record_features.push(feature_value);
+                }
+            }
+            data_rows.push(record_features);
+        }
+
+        // Convert the accumulated data into a matrix for the dataset struct.
+        let flattened_data: Vec<X> = data_rows.into_iter().flatten().collect();
+        let data = Matrix::new(
+            data_rows.len(),
+            data_rows[0].len(),
+            flattened_data
+        );
+
+        Ok(Dataset {
+            data,
+            target: Vector::new(target_values),
+            data_columns: headers.iter().map(|s| s.to_string()).collect(),
+            target_column: String::from(target_column),
+        })
     }
 }
