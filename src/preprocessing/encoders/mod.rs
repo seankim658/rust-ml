c302f503eaff8b9370b70de82bec1262dab260df
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-04-21 18:40:32 -0400

    update documentation for finished label encoder

--- src/preprocessing/encoders/mod.rs
+++ src/preprocessing/encoders/mod.rs
@@ -1,4 +1,10 @@
-//! Encoder module
+//! # Encoder module
+//!
+//! The module for the data encoders.
+//!
+//! ## Features
+//! - Label Encoder 
 //!
 
+/// Module for the label encoder.
 pub mod labelencoder;
